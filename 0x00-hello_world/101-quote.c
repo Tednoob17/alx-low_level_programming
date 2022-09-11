3990decc5d67442d272c707e76c37065d0bf1333
Tednoob17 <teddams047@gmail.com>
2022-09-11 19:24:12 +0100

    yo server

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print a hard sentences
+ *
+ * Return:  0 on success.
+ */
+int main(void)
+{
+	
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",107);
+
+
+
+
+
+return(1);
+}
