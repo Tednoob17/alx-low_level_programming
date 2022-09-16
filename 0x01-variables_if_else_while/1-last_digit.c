7a2fc60a551f64db9d326da6bd79fc934255af19
Tednoob17 <teddams047@gmail.com>
2022-09-16 11:21:21 +0100

    re-re-reee

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,17 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if (((n % 10) < 6) && ((n % 10) > 0))
+	else if (((n % 10) < 6) && ((n % 10) != 0))
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else
+	else if (n % 10 == 0)
 	{
 	printf("Last digit of %d is 0 and is %d", n, n % 10);
+	}
+	else
+	{
+		return (0);
 	}
 	return (0);
 }
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main -Entry block ( Print only the smallest combination of two digits)
 *
@@ -9,22 +8,22 @@ int main(void)
 {
 int n1 = 48, n2 = 49;
 
-while (n1 <= 56)
-{
-	while (n2 <= 57)
+	while (n1 <= 56)
 	{
-		putchar (n1);
-		putchar (n2);
-		if (n1 == 56 && n2 == 57)
+		while (n2 <= 57)
 		{
-		break;
+			putchar(n1);
+			putchar(n2);
+			if (n1 == 56 && n2 == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			n2++;
 		}
-		putchar (',');
-		putchar (' ');
-		n2++;
+			n1++;
+			n2 = n1 + 1;
 	}
-	n1++;
-	n2 = n1++;
-}
 return (0);
 }
