567d32a74a9634904e8575d2362b1c58fd700195
Tednoob17 <teddams047@gmail.com>
2022-09-16 11:29:11 +0100

    junkie css lol

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main -Entry block ( Print only the smallest combination of two digits)
 *
@@ -9,31 +8,30 @@ int main(void)
 {
 int n1 = 48, n2 = 49, n3 = 50;
 
+
 while (n1 <= 55)
 {
 	while (n2 <= 56)
 	{
-	       	     while (n3 <= 57)
-	             {
-			putchar (n1);
-			putchar (n2);
-			putchar (n3);
-				if (n1 == 55 && n2 == 56 && n3 == 57)
-				{
-					break;
-				}
-			putchar (',');
-			putchar (' ');
+		while (n3 <= 57)
+		{
+			putchar(n1);
+			putchar(n2);
+			putchar(n3);
+			if (n1 == 55 && n2 == 56 && n3 == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
 			n3++;
-	             }
+		}
 			n2++;
-			n3 = n2++;
+			n3 = n2 + 1;
 	}
-	n1++;
-	n2 = n1++;
-	n3 = n2++;
-	
-}
-/* 012-019-023*/
+		n1++;
+		n2 = n1 + 1;
+		n3 = n2 + 1; 	
+}	
 return (0);
 }
