4e3ef4c70e3d029f3fc95124e435c10a48d3b2da
Tednoob17 <teddams047@gmail.com>
2022-09-22 14:38:44 +0100

    adrerde

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ *
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int a = 48;
+	while(a <= 57)
+	{
+		if(a == 50 || a == 52)
+		{
+		break;
+		}
+	_putchar(a);
+	a++;
+	}
+	_putchar('\n');
+}
