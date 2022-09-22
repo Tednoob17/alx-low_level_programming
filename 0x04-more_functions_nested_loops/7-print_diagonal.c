78529e3cffae48f4d2e7ef7a53bbdbc1e40e03d1
Tednoob17 <teddams047@gmail.com>
2022-09-22 20:34:50 +0100

    yorushi

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,32 @@
 #include "main.h"
 /**
-* print_line - print backslash in function of n number
-* @n : number
+* print_diagonal - display backslash in function of n number
+* @n: number
 * Return: Void
 */
 void print_diagonal(int n)
 {
 	int i = 0;
+	int m = 0;
 
-		while (i < n)
+		while (i <= n)
 		{
+			while (m <= i)
+			{
+				_putchar(' ');
+				m++;
+			}
+			if (n <= 0)
+			{
+				_putchar('\n');
+			}
+			m = 0;
 			_putchar(92);
 			_putchar('\n');
 			i++;
 		}
-		i = 0;
-		_putchar('\n');
+		if (n < 0)
+		{
+			_putchar('\n');
+		}
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - print the square with #
+ * @size : number of # to be printed
+ * Return:void
+ */
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
+
