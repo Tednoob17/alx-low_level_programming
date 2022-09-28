1e5cebc53e7f4094bc1324818602a1dfae4f01c2
Tednoob17 <teddams047@gmail.com>
2022-09-28 09:08:01 +0100

    last

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,17 @@
 void _puts(char *str)
 {
 	int i = 0;
-
+	int m = 0;
 	if (*str)
 	{
 		while (str[i] != '\0')
-		{
+		{	
+			m++;	
 			_putchar(str[i]);
 			i++;
 		}
 		_putchar('\n');
-		if (*str=="")
+		if (m == 0 )
 		{
 			_putchar('\n');
 		}
