28203659ed79bee61b8deea336c926f414a4d33e
Tednoob17 <teddams047@gmail.com>
2022-09-28 14:16:56 +0100

    re brush

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,29 @@
- #include "main.h" 
+#include "main.h"
+/**
+ * puts_half - print half of char in parameters
+ *
+ * @str: pointer
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int m = 0;
+	int temp = 0;
+
+	while (str[i] != '\0')
+	{
+		m++;
+		i++;
+	}
+		temp = m / 2;
+		if (m % 2 == 0)
+		{
+			while (str[i] != '\0')
+			{
+				_putchar(str[temp]);
+				temp++;
+			}
+		}
+		_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 char *str;
-str = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
-puts2(str);
+str = "0123456789";
+puts_half(str);
 return (0);
 }
