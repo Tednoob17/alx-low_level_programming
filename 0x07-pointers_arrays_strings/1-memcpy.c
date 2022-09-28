db520edd62ed520922210dd956e7ab5f9a18d7a8
Tednoob17 <teddams047@gmail.com>
2022-09-28 01:11:15 +0100

    end i trust

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * *_memcpy -  copies memory area from src to dest
  * @dest: destination
@@ -7,8 +6,6 @@
  * @n: max bytes to use
  * Return: dest
  */
-
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,3 @@
-
-#include "main.h"
-
-/**
- * *_strchr - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @c: constant
- * Return: s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i;
-
-	for (i = 0; s[i] >= '\0'  ; i++)
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-	}
-
-	return ('\0');
-}
-
 #include "main.h"
 /**
  * _strchr - locates a character in a string,
@@ -32,13 +7,15 @@ char *_strchr(char *s, char c)
  */
 char *_strchr(char *s, char c)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        for (; *(s + i) != '\0'; i++)
-                if (*(s + i) == c)
-                        return (s + i);
-        if (*(s + i) == c)
-                return (s + i);
-        return ('\0');
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+		if (*(s + i) == c)
+			return (s + i);
+		return ('\0');
 }
+
+
 
