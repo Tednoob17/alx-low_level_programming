f5e8e0236101468a20e3f16350519cf0e0e1a494
Tednoob17 <teddams047@gmail.com>
2022-09-30 02:42:24 +0100

    4 yeah 4 i

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 int main(void)
 {
-	_print_rev_recursion("\nColton Walker");
+	int n;
+
+	n= _strlen_recursion("\nColton Walker");
+	printf("%d\n",n);
 	return(0);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-int _putchar(char c);
+int  _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-
+int  _strlen_recursion(char *s);
 
 
 
