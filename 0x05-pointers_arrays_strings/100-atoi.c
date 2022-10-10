bb733478b7588433c5ed28c24834ef958935674b
Tednoob17 <teddams047@gmail.com>
2022-10-10 19:01:10 +0100

    0x0e ok?

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,32 +3,42 @@
  * _atoi - change the string in a int S
  * @s : a pointer in char
  *Return : a
- *
  */
 int _atoi(char *s)
 {
 	int i = 0;
+	int sign = 1;
 	int n = 0;
-	int sign  = 1;
 
-	while (s[i] != '\0')
+	while ( *(s + i) != '\0')
 	{
-		
-		if(s[i] >= 48 && s[i] <= 57)
-		{
-			if( n == 0)
-			{
-				n = s[i];
+	  if( *(s + i) == '-'  && ( *(s + i + 1) >= '0' && *(s + i + 1)  <= '9')  )
+	  {
+		        if (n == 0)
+		        {
+			  sign = -1;
+			  n = *(s + i);
 			}
-		
-		
-		
+		        else
+		        {
+			  n = (n * 10) + *(s + i);
+			}
 		}
-		
-		
-		
-		i++;
-		
+	  else if ( *(s + i) >= '0' && *(s + i)<= '9')
+	  {
+		  if (n == 0)
+		  {
+		    n = *(s + i);
+		  }
+		  else
+		  {
+		     sign = 1;
+		     n = (n * 10) + *s;
+		  }
+	 }
+	  else
+	      break;
 	}
-	return ( sign * n);
+	i++;
+      return (sign * n);
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,18 @@
- #include "main.h"
+	#include <stdio.h>
+	#include <time .h>
+	#include <stdlib>
+
+	int main (void)
+{	{
+	  int s;
+	  time_t t; int n;
+
+	  srand(unsigned int ) time(&t) ;
+	  while (s )
+
+	
+	
+	
+	
+	
+	}
