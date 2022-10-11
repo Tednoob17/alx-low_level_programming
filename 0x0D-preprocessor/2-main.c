acf6ac3cbd4a42db9ba77e0462cfd3f53f203d4c
Tednoob17 <teddams047@gmail.com>
2022-10-11 22:05:57 +0100

    perfect clean up

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main -printf file name
- *Return : Always 0
+ *Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
