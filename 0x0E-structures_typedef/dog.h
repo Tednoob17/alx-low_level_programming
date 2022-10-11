890559e72198562be00325b0db40f32080f44db3
Tednoob17 <teddams047@gmail.com>
2022-10-11 17:39:46 +0100

    dog & h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct who will doing store dog information
+ *@name : it's a name of dog
+ *@age : it's a age og=f dog
+ *@owner : the owner dog name
+ *
+ *Description : this struct call a dog type for who store her name
+ *her age and her owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
