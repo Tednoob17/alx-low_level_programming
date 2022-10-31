05d88b943429e7ef457e572f57de1d7dc78a26bf
Tednoob17 <teddams047@gmail.com>
2022-10-31 23:48:05 +0100

    main ?

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
-
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 /**
 *read_textfile
 *
@@ -6,8 +8,21 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+ FILE *fp;	
+int i = 0;
+int c;
+	fp = fopen("filename","r");
+	if (fp == NULL || filename == NULL){
+		return (0);	
+}
 
-
-
-
+		while (c =getc(fp) != EOF)
+		{
+			for (i = 0; i < letters; i++)
+			{
+				putchar(c);
+			}	
+			fclose(fp);
+		}
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
