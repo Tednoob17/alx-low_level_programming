479b8a1bd41e95222e3cb3328696830f52824b41
Tednoob17 <teddams047@gmail.com>
2022-11-01 20:44:20 +0100

    true ??

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,11 +7,11 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fp;
-ssize_t inp, out;
-char *c;
+	int fp;
+	ssize_t inp, out;
+	char *c;
 
-fp = open(filename, O_RDONLY);
+	fp = open(filename, O_RDONLY);
 
 	if (fp == -1)
 		return (0);
@@ -23,11 +23,11 @@ fp = open(filename, O_RDONLY);
 	if (!c)
 		return (0);
 
-
 	inp = read(fp, c, letters);
 	out = write(STDOUT_FILENO, c, inp);
 
 	close(fp);
 	free(c);
+
 	return (out);
 }
