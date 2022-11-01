c399e442e441df905daff71c77deea503fb94fe3
Tednoob17 <teddams047@gmail.com>
2022-11-01 20:06:56 +0100

    brush bloom

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,33 @@
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
+#include "main.h"
 /**
-*read_textfile
-*
-*
+*read_textfile - read a text file and and print letters
+*@filename: It's a name file that we use
+*@letters:  It's a numbres of char when we can display
+*Return: Number of char printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
- FILE *fp;	
-int i = 0;
-int c;
-	fp = fopen("filename","r");
-	if (fp == NULL || filename == NULL){
-		return (0);	
-}
+int fp;
+ssize_t inp, out;
+char *c;
 
-		while (c =getc(fp) != EOF)
-		{
-			for (i = 0; i < letters; i++)
-			{
-				putchar(c);
-			}	
-			fclose(fp);
-		}
-	return (0);
+fp = open(filename, O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	else if (!(filename))
+		return (0);
+
+	c = malloc(sizeof(char) * (letters));
+	if (!c)
+		return (0);
+
+
+	inp = read(fp, c, letters);
+	out = write(STDOUT_FILENO, c, inp);
+
+	close(fp);
+	free(c);
+	return (out);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
