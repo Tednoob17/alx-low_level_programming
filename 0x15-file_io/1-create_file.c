d46b3e673a0d1ad25cbf61cc99789b8739d82104
Tednoob17 <teddams047@gmail.com>
2022-11-03 04:45:07 +0100

    1 -file_create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ *create_file - 
+ *@filename: is the name of the file create
+ *@text_content: is a NULL terminating string to write to the file
+ *
+ */
+
+int create_flie(const char *filename, char *text_content)
+{
+
+	if (!filename)
+		return (-1);
+
+
+
+
+
+
+}
