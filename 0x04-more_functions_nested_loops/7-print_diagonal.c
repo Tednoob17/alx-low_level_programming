0abbeb138062fc6b8e62edf890a967ae84b47347
Tednoob17 <teddams047@gmail.com>
2022-12-29 23:35:28 +0100

    loka

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,24 +9,19 @@ void print_diagonal(int n)
 	int i = 0;
 	int m = 0;
 
-		while (i <= n)
-		{
-			while (m <= i)
-			{
-				_putchar(' ');
-				m++;
-			}
-			if (n <= 0)
-			{
-				_putchar('\n');
-			}
-			m = 0;
-			_putchar('$');
-			_putchar('\n');
-			i++;
-		}
-		if (n < 0)
-		{
-		_putchar(' ');
-		}
+	if (n > 0)
+	{
+	  while(i < n){
+	    while(m < i+1){
+	      _putchar(' ');
+	      m++;
+	    }
+	    m = 0;
+	    _putchar(92);
+	    _putchar('\n');
+	      i++;
+	  }
+        }
+	else
+	  _putchar('\n');
 }
