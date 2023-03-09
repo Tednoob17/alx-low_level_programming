6ca17dfe35e761556812a83a963dc0a1b28c91a8
Tednoob17 <teddams047@gmail.com>
2023-03-09 21:53:14 +0100

    11-98 check

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,25 +20,25 @@ void print_to_98(int n)
 			 n++;
 		}
 	}
-	   else if (n > 98)
-	   {
-	         while (n >= 98)
+	else if (n > 98)
+	{
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n == 98)
-			    {break; }
+			{break; }
 			printf(",");
 			printf(" ");
 			n--;
-		 }
-	  }
+		}
+	}
 	else
 	{
-	while (n <= 98)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n == 98)
-				{break; }
+			{break; }
 			printf(",");
 			printf(" ");
 			n--;
